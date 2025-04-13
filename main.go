b9603e1a3ca11877072b1e2e9e37ab682788e33a
samirhembrom <samirhembrom007@gmail.com>
2025-04-13 18:07:12 +0530

    ch2_l5

--- main.go
+++ main.go
@@ -40,6 +40,7 @@ func main() {
 	cmds.register("login", handlerLogin)
 	cmds.register("register", handlerRegister)
 	cmds.register("reset", handlerReset)
+	cmds.register("users", handlerShowAllUsers)
 
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command> [args...]")
